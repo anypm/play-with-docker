c157a789b18b24755145ac6d3b9be2962b9d6428
Lei Xu <leixu@lean-soft.cn>
2018-02-19 16:56:53 +0800

    more pages to be localize and disable k8s for now

--- api.go
+++ api.go
@@ -25,7 +25,7 @@ func main() {
 	e := initEvent()
 	s := initStorage()
 	df := initDockerFactory(s)
-	kf := initK8sFactory(s)
+	//kf := initK8sFactory(s)
 
 	ipf := provisioner.NewInstanceProvisionerFactory(provisioner.NewWindowsASG(df, s), provisioner.NewDinD(id.XIDGenerator{}, df, s))
 	sp := provisioner.NewOverlaySessionProvisioner(df)
@@ -37,8 +37,8 @@ func main() {
 		task.NewCheckSwarmPorts(e, df),
 		task.NewCheckSwarmStatus(e, df),
 		task.NewCollectStats(e, df, s),
-		task.NewCheckK8sClusterStatus(e, kf),
-		task.NewCheckK8sClusterExposedPorts(e, kf),
+		//task.NewCheckK8sClusterStatus(e, kf),
+		//task.NewCheckK8sClusterExposedPorts(e, kf),
 	}
 	sch, err := scheduler.NewScheduler(tasks, s, e, core)
 	if err != nil {
